6198c5d9742f376dae35f82f92ea3036fb86b89b
Kyler <Krenneker16@gmail.com>
2019-06-25 19:44:41 -0400

    added to sign up form

--- src/components/HandymanSignUpForm/HandymanSignUpForm.js
+++ src/components/HandymanSignUpForm/HandymanSignUpForm.js
@@ -35,17 +35,30 @@ export default function HandymanSignUpForm(props) {
                     id='HandymanSignUpForm__user_name'>
                 </input>
             </div>
-            <div className='providerDisplay_name'>
-                <label htmlFor='HandymanSignUpForm__user_name'>
-                    User name
+            <div className='display_name'>
+                <label htmlFor='HandymanSignUpForm__display_name'>
+                    Your Handyman display name
                 </label>
                 <input
                     name='user_name'
                     type='text'
                     required
-                    id='HandymanSignUpForm__user_name'>
+                    id='HandymanSignUpForm__display_name'>
                 </input>
             </div>
+            <div className='location'>
+                <label></label>
+                <input></input>
+            </div>
+            <div className='handyman__services'>
+                <label></label>
+                <input></input>
+            </div>
+            <div className='password'>
+                <label></label>
+                <input></input>
+            </div>
+
 
         </form>
     )
