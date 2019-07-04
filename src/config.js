fcf97e8f28181748c2967e5668332fe7e2144b3d
Kyler <Krenneker16@gmail.com>
2019-07-04 16:34:47 -0400

    added api key envlocal file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'handyman-client-auth-token'
+    API_ENDPOINT: 'https://git.heroku.com/peaceful-kobuk-valley-49274.git',
+    API_KEY: process.env.REACT_APP_API_KEY
 }
